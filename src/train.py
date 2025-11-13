08de41bf84683748f4b38447b62fa77dbe6d39a7
MarcusBluestone <169070970+marcusbl1@users.noreply.github.com>
2025-11-13 18:33:50 -0500

    train overview

--- src/train.py
+++ src/train.py
@@ -2,6 +2,17 @@ from src.data import CleanDataset, CleanDatasetIterable
 from src.data import NoiseGenerator
 
 def train():
+
+    """
+    1. Sample chunk
+    2. Add noise to chunk
+    3. Convert into spectrogram
+    4. Reshape to (256, 256)
+    5. Input into Model
+    6. Reshape output back into original size
+    7. Loss = MSE btwn spectrograms
+    8. Cann also convert spectrogram -> waveform -> audiofile if you want to hear what it actually sounds like  
+    """
     pass
 
 if __name__ == '__main__':
